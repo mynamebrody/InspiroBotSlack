35cafe4dc8e7453c88666e2dc9fa515bae4a4520
Brody Berson <brody.berson@me.com>
2015-08-14 13:45:52 -0600

    Added Inspirobot.met endpoint logic

--- generate.js
+++ generate.js
@@ -1,13 +1,16 @@
 module.exports = function (req, res, next) {
-  var userName = req.body.user_name;
+  var request = require('request');
+  var image = 'http://inspirobot.me/website/images/inspirobot-dark-green.png';
+  request('http://inspirobot.me/api?generate=true', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      image = body
+    }
+    else {
+      return res.status(200).end();
+    }
+  })
   var botPayload = {
-    text : 'Hello, ' + userName + '!'
+    text : image
   };
-
-  // avoid infinite loop
-  if (userName !== 'slackbot') {
-    return res.status(200).json(botPayload);
-  } else {
-    return res.status(200).end();
-  }
+  return res.status(200).json(botPayload);
 }
