0ca113c2320ce6a347d3da19a5c8e7ee10577183
Brody Berson <brody.berson@me.com>
2015-08-14 13:27:10 -0600

    Rename to generate

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var hellobot = require('./hellobot');
+var generate = require('./generate');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -21,4 +21,4 @@ app.listen(port, function () {
   console.log('Slack bot listening on port ' + port);
 });
 
-app.post('/hello', hellobot);
+app.post('/inspirobot', generate);
