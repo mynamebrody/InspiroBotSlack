0156cf8d5fe22f215ea8fe76b3f8c2ec281aa801
Brody Berson <brody.berson@me.com>
2015-08-14 11:06:57 -0600

    Creation of app.js and hellobot.js

---
+++ hellobot.js
@@ -0,0 +1,13 @@
+module.exports = function (req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Hello, ' + userName + '!'
+  };
+
+  // avoid infinite loop
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+}
