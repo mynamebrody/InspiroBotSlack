3b072d388be6907bdb93894b8971da67a7c1c5c0
Brody Berson <brody.berson@me.com>
2015-08-14 14:56:51 -0600

    Error handling around InspiroBot's API to return default image

--- generate.js
+++ generate.js
@@ -1,16 +1,18 @@
 module.exports = function (req, res, next) {
   var request = require('request');
-  var image = 'http://inspirobot.me/website/images/inspirobot-dark-green.png';
+  var errimage = 'http://inspirobot.me/website/images/inspirobot-dark-green.png';
   request('http://inspirobot.me/api?generate=true', function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      image = body
+      var botPayload = {
+        text : body
+      };
+      return res.status(200).json(botPayload);
     }
     else {
-      return res.status(200).end();
+      var botPayload = {
+        text : errimage
+      };
+      return res.status(200).json(botPayload);
     }
   });
-  var botPayload = {
-    text : image
-  };
-  return res.status(200).json(botPayload);
 }
