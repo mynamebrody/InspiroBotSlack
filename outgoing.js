35315aace37bee80c1ec5880ce7a9cc6e747fc1f
Brody Berson <brody.berson@me.com>
2015-08-16 12:06:58 -0600

    Refactor to cleanup

--- outgoing.js
+++ outgoing.js
@@ -1,17 +1,15 @@
+var request = require('request');
+
 module.exports = function (req, res, next) {
-  var request = require('request');
   var errimage = 'http://inspirobot.me/website/images/inspirobot-dark-green.png';
   request('http://inspirobot.me/api?generate=true', function (error, response, body) {
+    var botPayload = {};
     if (!error && response.statusCode == 200) {
-      var botPayload = {
-        text : body
-      };
+      botPayload.text = body;
       return res.status(200).json(botPayload);
     }
     else {
-      var botPayload = {
-        text : errimage
-      };
+      botPayload.text = errimage;
       return res.status(200).json(botPayload);
     }
   });
